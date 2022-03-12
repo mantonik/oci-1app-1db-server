c5fe677bee0f63708bf104cdf77072e2256a152c
Mariusz Antonik <mantonik@gmail.com>
2022-03-12 15:40:08 -0500

    Initial version

---
+++ server-config/bin/01.install-server.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Script will perform instalation of the app and db servers 
+#Scritp should be executed as root user, 
+# check if user is root
+
+#Run main instalation on each server 
+version=1.1
+
+echo "Run instalation script"
+if [ "$HOSTNAME" == *"app1"* ] ; then
+  echo "Install Web server "
+  sudo /home/opc/bin/02.server-instalation-script-app.sh
+
+
+fi
+sudo /home/opc/bin/02.server-instalation-script-app.sh
+
+#Run DB instalation on app2 and app4 only 
+
+if [ "$HOSTNAME" == *"app2"* ] || [ "$HOSTNAME" == *"app4"* ] ; then
+  echo "Run MySQL server instalation "
+  sudo /home/opc/bin/03.server-instalation-mysql.sh
+fi
+
+echo "---------------------------"
+echo "Version: ${version}"
+echo "---------------------------"
