46c449dcf270809df43df721b0ac1e68cb026084
Mariusz Antonik <mantonik@gmail.com>
2022-03-12 17:58:15 -0500

    update

--- server-config/bin/03.server-instalation-mysql.sh
+++ server-config/bin/03.server-instalation-mysql.sh
@@ -183,7 +183,7 @@ fi
 if [[ "$HOSTNAME" == *"app4"* ]] ; then
   mount -t nfs 10.10.1.14:/share /mnt/share_app4
   mount -t nfs 10.10.1.12:/share /mnt/share_app2
-fi
+fi 
 
 
 echo "Host app2 or app4 - install MySQL"
