512e8b98e4298960b32c1a736b27f61b2ab6d5b5
Mariusz Antonik <mantonik@gmail.com>
2022-03-13 00:53:11 -0500

    add install WP

---
+++ server-config/bin/install_wp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Enter Domain Name 
+echo "Enter domain name: "
+read DOMAINNAME
+
+ROOTDIRPATH="/data/www/domain/${DOMAINNAME}/htdocs"
+
+
+echo "Configure ngixn for domain: "${DOMAINNAME}
+echo "ROOT directory : "${ROOTDIRPATH}
+
+#Update nginx configuration 
+sed "s/DOMAIN-NAME/${DOMAINNAME}/g" < /etc/nginx/conf.d/0.template.wp.conf.txt > /etc/nginx/conf.d/${DOMAINNAME}.conf
+sed "s/ROOT-DIR-PATH/${ROOTDIRPATH}/g" -i /etc/nginx/conf.d/${DOMAINNAME}.conf
+
+echo "Setup a nginx configuration for domain"
+#prepare file system for new domain 
+
+mkdir -p ${ROOTDIRPATH}
+
+#Create folder structure for certbot
+
+mkdir -p ${ROOTDIRPATH}/htdocs/.well-known
+echo $HOSTNAME > ${ROOTDIRPATH}/htdocs/.well-known/index.html
+
+# Create nginx configuration for domain
+chown -R nginx:nginx /data/www/
+service nginx restart
+
+#setup wordpress
+echo " Download latest WP "
+cd /root/install
+wget https://wordpress.org/latest.tar.gz
+tar xvf latest.tar.gz
+cd /root/install/wordpress
+
+echo "Copy to domain folder"
+cp -r * ${ROOTDIRPATH}/htdocs
+ls -l ${ROOTDIRPATH}/htdocs
+echo "${DOMAINNAME} is up" > ${ROOTDIRPATH}/htdocs/test.html
+
+chown -R nginx:nginx /data/www/
+find /data/www -type d -exec chmod 750 {} \;
+find /data/www -type f -exec chmod 640 {} \;
+
+exit
+
+#dmcloudarchitect.com
+#support
+# Ujh7834%2#wdx)9plkjHns$
+
+
